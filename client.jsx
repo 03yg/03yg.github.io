7bf01f8d1f2c6721af62151d4b1ebfd03e16badf
03yg <03.yg.jung@gmail.com>
2021-07-20 15:27:52 +0900

    변수 WordRelay 를 Stock 으로 수정

--- client.jsx
+++ client.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const Stock = require('./src/Stock');
+const Stock = require('/src/Stock');
 
 ReactDom.render(<Stock />, document.querySelector('#root'));
